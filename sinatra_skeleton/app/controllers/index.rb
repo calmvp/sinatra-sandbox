88cd824de3f24b5b7bd5064efa14a4ec3ebdd25e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-06 15:47:36 -0500

    Not the bees

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -13,5 +13,12 @@ end
 post '/cool_url' do
   puts "[LOG] Getting /cool_url"
   puts "[LOG] Params: #{params.inspect}"
-  erb :post_cool_url
+  @text_input = params[:text_input]
+  @area_input = params[:myname]
+  @radio_button2 = params[:group2]
+  if @radio_button2 == "Bees"
+    erb :get_cool_url 
+  else
+    erb :post_cool_url
+  end
 end
